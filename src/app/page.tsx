21aa651532199e4ae296c76aad5c39f7867812b4
gabrielmoraesy <ygabrielmoraes@gmail.com>
2024-09-30 13:26:16 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="w-full h-[90vh] flex flex-col justify-center items-center">
+      <h1 className="text-[100px] text-blue-600">MoraesDev</h1>
+      <p className="text-[50px]">Home - Projeto Teste</p>
+    </div>
+  );
+}
